1d7300c6a59f2083e2d2382464abc9388ad3a25a
Papa Nii Christian Vanderpuye <36800450+papaniivanderpuye@users.noreply.github.com>
2018-07-19 01:21:48 -0400

    Update ctNotes.js

--- ctNotes.js
+++ ctNotes.js
@@ -1,44 +1,80 @@
-class Button extends React.Component {
-  state = { counter: 1, bgColor:"red"
-  };
-  
-  handleClick = () => {
-    this.setState((prevState) => ({
-      counter: prevState.counter + 1,
-      bgColor: 'blue'
-      
-    }));
-  };
-  
-  render() {
-    return (
-      
-      <div>
-        <button 
-          onClick={this.handleClick} 
-          style={{backgroundColor:this.state.bgColor}}>Button
-          </button>
-      </div>
-    );
-  }
+class Question extends React.Component {
+              state = { quest: "what is life?"};
+
+              handleClick = () => {
+                this.setState((prevState) => ({
+
+                }));
+              };
+
+              render() {
+                return (
+
+                  <div>
+                    <h1
+                      >{this.state.quest}
+                    </h1>
+                  </div>
+                );
+              }
 }
+
+class Button extends React.Component {
+              state = { counter: 1, bgColor:"red"
+              };
+
+              handleClick = () => {
+                this.setState((prevState) => ({
+                  counter: prevState.counter + 1,
+                  bgColor: 'blue'
+
+                }));
+              };
+
+              render() {
+                return (
+
+                  <div>
+                    <button 
+                      onClick={this.handleClick} 
+                      style={{backgroundColor:this.state.bgColor}}
+                      >{this.state.counter}
+                    </button>
+                  </div>
+                );
+              }
+}
+
+
 
 class Grid extends React.Component {
   state = { counter: 1 };
   render() {
     return (
     	<div>
-        <Button 
-        onClick={this.handleClick}
-        >
-          {this.state.counter}
-        </Button>
-        <Button onClick={this.handleClick}>
-          {this.state.counter}
-        </Button>
-        <Button onClick={this.handleClick}>
-          {this.state.counter}
-        </Button>
+      	<Question>
+        </Question>
+  
+  			<div id = "grid-container">	
+        	<div class="action-container">	
+            <Button >
+            </Button>
+          </div>
+          <div class="action-container">	
+            <Button >
+            </Button>
+          </div>
+          <div class="action-container">	
+            <Button >
+            </Button>
+          </div>
+          <div class="action-container">	
+            <Button >
+            </Button>
+          </div>
+          
+        </div>
+        
       </div>
     );
   }
